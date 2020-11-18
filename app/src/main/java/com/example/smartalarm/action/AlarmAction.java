a1fbdd89322cda1f20b2c428cf92abcb5a9b2806
bliminate <blimmer@umich.edu>
2020-11-18 00:45:10 -0700

    alarm action and renamed resources

--- app/src/main/java/com/example/smartalarm/action/AlarmAction.java
+++ app/src/main/java/com/example/smartalarm/action/AlarmAction.java
@@ -1,22 +1,66 @@
 package com.example.smartalarm.action;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
+import com.example.smartalarm.R;
+
 import java.beans.PropertyChangeEvent;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static android.content.Context.VIBRATOR_SERVICE;
 
 public class AlarmAction extends Action {
-   public AlarmAction(){
+   public AlarmAction(Context ctx){
       super();
       vibrate = false;
       soundResource = 0;
+      threads = null;
+      context = ctx;
    }
 
    @Override
    public void executeAction() {
+      if(threads == null) {
+         threads = Executors.newFixedThreadPool(2);
+         if (vibrate) {
+            //vibrate phone
+            threads.submit(new Runnable() {
+               @RequiresApi(api = Build.VERSION_CODES.O)
+               @Override
+               public void run() {
+                  Vibrator vibrate = (Vibrator) context.getSystemService(VIBRATOR_SERVICE);
+                  vibrate.vibrate(VibrationEffect.createWaveform(new long[]{100,200,100}, new int[]{500, 400, 500}, 2));
+               }
+            });
+         }
 
+         if (soundResource != 0) {
+            //play sound in background thread
+            threads.submit(new Runnable() {
+               @Override
+               public void run() {
+                  MediaPlayer media = MediaPlayer.create(context, soundResource);
+                  media.start();
+               }
+            });
+         }
+      }
    }
 
    @Override
    public void stopAction() {
-
+      if(threads != null){
+         threads.shutdownNow();
+         threads = null;
+      }
    }
 
    @Override
@@ -55,4 +99,6 @@ public class AlarmAction extends Action {
    private String name;
    private Boolean vibrate;
    private Integer soundResource;
+   private ExecutorService threads;
+   private Context context;
 }

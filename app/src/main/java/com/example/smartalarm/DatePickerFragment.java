014ff67d2946beb8ca9e10ebcb63b87555c0fbae
kandai <kandai.wata@gmail.om>
2020-11-18 22:04:26 +0900

    Auto stash before merge of "UI" and "origin/UI"

---
+++ app/src/main/java/com/example/smartalarm/DatePickerFragment.java
@@ -0,0 +1,32 @@
+package com.example.smartalarm;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), (AddEventActivity)getActivity(), year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+    }
+}
--- app/src/main/java/com/example/smartalarm/MainActivity.java
+++ app/src/main/java/com/example/smartalarm/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.smartalarm;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+   private static final String LOG_TAG = MainActivity.class.getSimpleName();
+   public static final int TEXT_REQUEST = 1;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +17,29 @@ public class MainActivity extends AppCompatActivity {
       setContentView(R.layout.activity_main);
 
       
+   }
+
+   public void createAction(View view) {
+      Log.d(LOG_TAG, "Create Action Button clicked!");
+      Intent intent = new Intent(this, AddActionActivity.class);
+      startActivity(intent);
+   }
+
+   public void createEvent(View view) {
+      Log.d(LOG_TAG, "Create Event Button clicked!");
+      Intent intent = new Intent(this, AddEventActivity.class);
+      startActivityForResult(intent, TEXT_REQUEST);
+   }
+
+   @Override
+   public void onActivityResult(int requestCode,
+                                int resultCode, Intent data) {
+      super.onActivityResult(requestCode, resultCode, data);
+      if (requestCode == TEXT_REQUEST) {
+         if (resultCode == RESULT_OK) {
+            Event event = (Event)data.getSerializableExtra(AddEventActivity.CREATED_EVENT);
+            Log.d(LOG_TAG, event.toString());
+         }
+      }
    }
 }
